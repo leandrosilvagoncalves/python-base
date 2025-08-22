899b0f8d1eda282462ffd78e76582e8ed9cf3bc2
Leandro Goncalves <leandro.goncalvez@gmail.com>
2025-08-22 12:07:43 +0400

    Implemented Multilanguage Hello World

--- hello.py
+++ hello.py
@@ -1,6 +1,38 @@
 #! /usr/bin/env python3
+"""Multilanguage Hello World.
 
-#This prints Hello World
+Depending on language defined on systems environment variables, the program will print "Hello World" in the specified language.
 
-print("Hello World!!")
+Usage:
+You must have LANG set in your environment variables. example:
+ export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    or
+    /hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Leandro Goncalves"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, le Monde!"
+elif current_language == "es_ES":
+    msg = "¡Hola, Mundo!"
+    
+
+print(msg)
+
 
